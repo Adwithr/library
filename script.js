e641d15a1d5f83d81a9cd04d02e7e774cd26c739
Adwithr <154674132+Adwithr@users.noreply.github.com>
2024-07-14 00:07:21 +0530

    dialog

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-let statusChangeBtn = document.querySelector(".status-change-btn");
-let statusRn = document.querySelector("#status-rn");
-let statusChangeBtnStatus = document.querySelector("#status-change-btn-status");
+const addNewBookBtn = document.querySelector(".new-book-btn");
+const dialogBox = document.querySelector("dialog");
+const cancel = document.querySelector("#cancel");
 
-statusChangeBtn.addEventListener("click", () => {
-  if (statusRn.textContent == "Read") {
-    statusChangeBtnStatus.textContent = "Read";
-    statusRn.textContent = "Unread";
-  } else {
-    statusChangeBtnStatus.textContent = "Unread";
-    statusRn.textContent = "Read";
-  }
-}); // DOESNT WORK ON MULTIPLE CARDS
+addNewBookBtn.addEventListener("click", () => {
+  dialogBox.showModal();
+});
+
+cancel.addEventListener("click", () => {
+  dialogBox.close();
+});
